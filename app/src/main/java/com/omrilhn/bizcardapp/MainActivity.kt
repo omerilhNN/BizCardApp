96a35bc48d85797788364540b4924203a0a868b3
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-02 21:14:30 +0300

    mutableState for toggle button

--- app/src/main/java/com/omrilhn/bizcardapp/MainActivity.kt
+++ app/src/main/java/com/omrilhn/bizcardapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.omrilhn.bizcardapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +17,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
@@ -27,6 +32,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -61,6 +68,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBizCard()
 {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -73,15 +83,62 @@ fun CreateBizCard()
             colors  = CardDefaults.cardColors(
                 containerColor = Color.White))
             {
-                Column(modifier = Modifier.height(300.dp).width(400.dp),
+                Column(modifier = Modifier
+                    .height(300.dp)
+                    .width(400.dp),
                     verticalArrangement = Arrangement.Top,
                     horizontalAlignment = Alignment.CenterHorizontally){
 
                     CreateImageProfile()
                     Divider(thickness = 0.5.dp, color = Color.Blue)
                     CreateInfo()
+                    Button(onClick = {
+                        buttonClickedState.value  != buttonClickedState.value //When its true set it to false vise versa
+                    }) {
+                        Text("Portfolio",
+                            style = MaterialTheme.typography.labelSmall)
+                    }
+                    if(buttonClickedState.value)
+                    {
+                        Content()
+                    }else{
+                        Box(){
+                            
+                        }
+                    }
                 }
             }
+    }
+}
+@Preview
+@Composable
+fun Content()
+{
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(5.dp)){
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxHeight()
+            .fillMaxWidth(),
+            shape = RoundedCornerShape(6.dp),
+            border = BorderStroke(width = 2.dp,color = Color.LightGray)){
+            Portfolio(data = listOf("Project1","Project2","Project3",
+                "Project3",
+                "Project3"  ))
+
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data){ item ->
+            //item: every one of the elements
+            Text(item)
+        }
     }
 }
 
@@ -131,7 +188,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier)//With that paramet
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BizCardAppTheme {
