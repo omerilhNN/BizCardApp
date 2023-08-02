a2277e1f5f5ad2ef33c2cc72f2562b5b7ff7a230
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-02 15:16:31 +0300

    CreateProfileImage and add divider

--- app/src/main/java/com/omrilhn/bizcardapp/MainActivity.kt
+++ app/src/main/java/com/omrilhn/bizcardapp/MainActivity.kt
@@ -3,26 +3,36 @@ package com.omrilhn.bizcardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.omrilhn.bizcardapp.ui.theme.BizCardAppTheme
@@ -60,12 +70,38 @@ fun CreateBizCard()
             elevation = CardDefaults.cardElevation(10.dp),
             shape = RoundedCornerShape(corner = CornerSize(10.dp)),
             colors  = CardDefaults.cardColors(
-                containerColor = Color.White,
-                contentColor = Color.Black)
-        )
-
+                containerColor = Color.White))
             {
-        }
+                Column(modifier = Modifier.height(300.dp).width(400.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally){
+
+                    CreateImageProfile()
+                    Divider()
+                }
+
+            }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier)//With that parameter type it is optional whether you add parameter or not
+ {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 5.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)//Set ALPHA on the foreground of the image
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
